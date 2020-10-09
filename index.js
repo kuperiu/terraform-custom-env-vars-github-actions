b3d5f8f617cb5c724c03b145dbd265bb861295bf
Lior <lior@l01015153.lan>
2020-10-09 11:19:01 +0200

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const sha = process.env.GITHUB_SHA.substring(0,10);
 const repoName = process.env.GITHUB_REPOSITORY.split('/');
 const key = repoName[1] + '/' + sha + '.zip';
 
-core.exportVariable('TF_KEY', key);
-core.info(`Set TF_KEY=` +
-`${process.env.TF_KEY}`);
+core.exportVariable('TF_VAR_key', key);
+core.info(`Set TF_VAR_key=` +
+`${process.env.TF_VAR_key}`);
+
